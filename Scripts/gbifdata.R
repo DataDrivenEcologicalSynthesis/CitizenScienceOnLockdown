c9233619975a63149ef2bd6fdff73b1d586198e8
Shuang Xing <46916708+shuanggeek@users.noreply.github.com>
2020-05-25 20:38:41 +0200

    updated data downloading Rscript

--- Scripts/gbifdata.R
+++ Scripts/gbifdata.R
@@ -5,8 +5,8 @@ library(rgbif)
 library(dplyr)
 ###for all the iNat bird data of Canada since 2020 from Janurary to May
 Canada_gbif <- occ_download_get(key = "0069150-200221144449610", overwrite = TRUE)  %>%  occ_download_import(Canada_gbif_download, na.strings = c("", NA))
-###for all the iNat bird data of Canada from Janurary to May from 2011-2020
-Canada_gbif <- occ_download_get(key = "0069154-200221144449610", overwrite = TRUE)  %>%  occ_download_import(Canada_gbif_download, na.strings = c("", NA))
+###for all the iNat bird data of Canada from Janurary to May from 2011-2019
+Canada_gbif <- occ_download_get(key = "0070513-200221144449610", overwrite = TRUE)  %>%  occ_download_import(Canada_gbif_download, na.strings = c("", NA))
 ##selecting those has been identified to species level
 Canada_occurrence <- Canada_gbif %>% filter(taxonRank == "SPECIES")
 ## selecting those has both longitute and latitude coordination
