152bbad697475d10bd6e00d9a8ad45b4a2340eb4
Melanie Jean <melanie.jean@uqat.ca>
2020-05-27 15:56:58 -0400

    Download of all bird data for April 2020

--- Scripts/01_Datascraping.R
+++ Scripts/01_Datascraping.R
@@ -23,3 +23,13 @@ test <- test[test$iconic_taxon_name=="Aves",]
 	# with Ebird ----
 test2 <- ebirdregion(loc="CA", key = "hth3fsfparb7", max=100)
 # get the 100 results
+
+
+# I. Downloading data take 2---- With the current filters I can download data for one month at the time. 
+# If we focus on just April and March and on the years >2015 it's possibily feasible
+# with Inaturalist ----
+test2 <- get_inat_obs(year = 2020
+					 , month = 4
+					 , geo = T
+					 , bounds = c(40, 130, 60, 52) # updated the coordinates to select just southern Canada
+					 ,taxon_name="Aves")# I think this last argument selects only birds
