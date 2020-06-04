e734ff39d1b0c1696c32a891fffcd3eef63f6701
shuanggeek <xingshuangbjfu@gmail.com>
2020-06-04 14:59:19 +0200

    updates

--- Scripts/Figures/SX_figures.R
+++ Scripts/Figures/SX_figures.R
@@ -10,37 +10,19 @@ install.packages("fossil")
 # Libraries
 library(dplyr)
 library(ggplot2)
-library(vegan)
-library(reshape2)
-library(fuzzySim)
+# library(vegan)
+# library(reshape2)
+# library(fuzzySim)
 library(fossil)
-data(BCI, package = "vegan")
-BCI2 <- BCI[1:26, ]
-data(rotifier)
-	 inat_clean_rarefaction<- inat_identity%>%
-	dplyr::group_by(observed_on)%>%
-	#dplyr::summarise(spc_richness = length(unique(scientific_name)))%>%
-	select(observed_on, scientific_name)
-	 
-	 inat_clean_rarefaction$freq<-1
-	 community<-aggregate(freq~scientific_name+observed_on,inat_clean_rarefaction,sum)
-	 A<-create.matrix(fdata.list, tax.name = "species", locality = "locality", 
-	 			  abund=TRUE, abund.col="abundance")
-	 A<-create.matrix(community, tax.name = "scientific_name", locality = "observed_on", 
-	 			  abund=TRUE, abund.col="freq")
-	 rarenew<-as.data.frame(t(A))
-	 rare<-rarenew%>%
-	 	filter(rowSums(rarenew)!=1)
-	#rare<- splist2presabs(inat_clean_rarefaction, sites.col = 1, sp.col = 2, keep.n = FALSE)
-	#rarenew<-rare[,2:387]
-	raremax <- min(rowSums(rare))
-	S <- specnumber(rare) # observed number of species
-		Srare <- rarefy(rare, raremax)
-		
-	plot(S, Srare, xlab = "Observed No. of Species", ylab = "Rarefied No. of Species")
-	abline(0, 1)
-	rarecurve(rare, step = 20, sample = raremax, col = "blue", cex = 0.6)
-biodiversity<-inat_clean_rarefaction
+# data(BCI, package = "vegan")
+# BCI2 <- BCI[1:26, ]
+# data(rotifier)
+
+# 		
+# 	plot(S, Srare, xlab = "Observed No. of Species", ylab = "Rarefied No. of Species")
+# 	abline(0, 1)
+# 	rarecurve(rare, step = 20, sample = raremax, col = "blue", cex = 0.6)
+# biodiversity<-inat_clean_rarefaction
 
 # end INIT ----
 
@@ -53,31 +35,48 @@ inat <- read.table("Data/04_Inat_from_clipping_NoDuplicates-csv.tsv"
 				   , comment.char = ""
 				   , na.strings=c("NA", "")
 				   , header=T)
-write.table(inat,"inat.csv",sep=",")
+#write.table(inat,"inat.csv",sep=",")
 #for checking all the record with duplicated "id"
 # 
 # 
-dup<-inat%>% 
-	group_by(id) %>% 
-filter(n() != 1)
-write.table(dup,"Data/dup.csv",sep=",")
+# dup<-inat%>% 
+# 	group_by(id) %>% 
+# filter(n() != 1)
+# write.table(dup,"Data/04_dup.csv",sep=",")
 
 #final data with unique id
 inat_identity<-distinct(inat, id, .keep_all= TRUE)
-write.table(inat_identity,"Data/inat_identity.csv",sep=",")
+#write.table(inat_identity,"Data/inat_identity.csv",sep=",")
 
 ###figures for quality_grade
 inat_clean_observation <- inat_identity%>%
 	dplyr::group_by(Ggrphc_n, year,quality_grade) %>%
 	dplyr::summarise(spc_richness = length(unique(scientific_name))
 					 , nb_observations = length(user_login))
-####quality composition for observation in each city
-jpeg("Figures/Hypothesis_1/SX_quality_grade_observation.jpg", width = 1000, height = 700)
-ggplot(inat_clean_observation,aes(x=year,y=nb_observations,fill=quality_grade))+
+quality_percent<-inat_clean_observation%>%
+	dplyr::group_by(Ggrphc_n, year) %>% 
+	mutate(percent_nb_observations  = nb_observations /sum(nb_observations),
+           percent_spc_richness   = spc_richness /sum(spc_richness ))
+
+####quality percentage composition for observation in each city
+jpeg("Figures/Hypothesis_1/SX_quality_grade_observation_percent.jpg", width = 1000, height = 700)
+ggplot(quality_percent,aes(x=year,y=percent_nb_observations,fill=quality_grade))+
       geom_bar(width = 1, stat = "identity")+
 	     	facet_wrap(~Ggrphc_n)
 dev.off()
-####quality composition for observers in each city
+####quality composition percentage  for species richness in each city
+jpeg("Figures/Hypothesis_1/SX_quality_grade_richness_percent.jpg", width = 1000, height = 700)
+ggplot(quality_percent,aes(x=year,y=percent_spc_richness,fill=quality_grade))+
+	geom_bar(width = 1, stat = "identity")+
+	facet_wrap(~Ggrphc_n)
+dev.off()
+####quality composition for observation in each city
+jpeg("Figures/Hypothesis_1/SX_quality_grade_observation.jpg", width = 1000, height = 700)
+ggplot(inat_clean_observation,aes(x=year,y=nb_observations,fill=quality_grade))+
+	geom_bar(width = 1, stat = "identity")+
+	facet_wrap(~Ggrphc_n)
+dev.off()
+####quality composition for species richness in each city
 jpeg("Figures/Hypothesis_1/SX_quality_grade_richness.jpg", width = 1000, height = 700)
 ggplot(inat_clean_observation,aes(x=year,y=spc_richness,fill=quality_grade))+
 	geom_bar(width = 1, stat = "identity")+
@@ -149,6 +148,51 @@ ggplot(inat_clean_time, aes(x =ym, y = spc_richness,group=1)) +
 	geom_line(aes(color=Ggrphc_n)) +
 	facet_wrap(~Ggrphc_n)+
 	theme_classic()+
+
 	theme(axis.text.x=element_text(angle=60, hjust=1)) 
 
 dev.off()
+## compare community
+inat_clean_rarefaction_time<- inat_identity%>%
+	dplyr::group_by(observed_on,quality_grade)%>%
+	#dplyr::summarise(spc_richness = length(unique(scientific_name)))%>%
+	select(observed_on, scientific_name,quality_grade,year)
+
+inat_clean_rarefaction_time$freq<-1
+community_time<-aggregate(freq~scientific_name+quality_grade+year,inat_clean_rarefaction_time,sum)
+communty_time_percent<-community_time%>%
+	dplyr::group_by(year,scientific_name) %>% 
+	mutate(percent_freq = freq /sum(freq),totalfreq=sum(freq))
+communty_time_percent$quarantine <- ifelse(communty_time_percent$year == 2020, "yes", "no")
+communty_time_percent$quarantine <- factor(communty_time_percent$quarantine)
+
+jpeg("Figures/Hypothesis_1/SX_time_speciesfreq.jpg", width = 1000, height = 700)
+ggplot(communty_time_percent,aes(x=totalfreq,y=percent_freq ))+
+geom_point(aes(color=quality_grade))+
+	facet_wrap(~quarantine)+
+theme_classic()+
+	xlab("Number of observations of a species")+
+	ylab("percentage of each quality grade")+
+	theme(axis.text.x=element_text(angle=60, hjust=1)) 
+write.table(communty_time_percent,"Data/speciesabundanceyear.csv",sep=",")
+dev.off()
+#####the obseration of frequncy for each species over years
+jpeg("Figures/Hypothesis_1/SX_time_speciesfreqyear.jpg", width = 1000, height = 700)
+ggplot(communty_time_percent,aes(x=year,y=totalfreq ))+
+	geom_point()+
+		theme_classic()+
+	xlab("year")+
+	ylab("species frequency")
+
+dev.off()
+
+A<-create.matrix(community, tax.name = "scientific_name", locality = "observed_on",
+				 abund=TRUE, abund.col="freq")
+timemt<-as.data.frame(t(A))
+rare<-rarenew%>%
+	filter(rowSums(rarenew)!=1)
+#rare<- splist2presabs(inat_clean_rarefaction, sites.col = 1, sp.col = 2, keep.n = FALSE)
+#rarenew<-rare[,2:387]
+raremax <- min(rowSums(rare))
+S <- specnumber(rare) # observed number of species
+Srare <- rarefy(rare, raremax)
