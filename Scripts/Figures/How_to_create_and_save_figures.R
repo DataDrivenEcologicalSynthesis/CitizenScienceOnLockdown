139599fdb703d783513b790520cb41c36045b7c0
AlienorStahl <a.stahl67@gmail.com>
2020-06-04 09:59:03 -0400

    Style of figure you should be using

--- Scripts/Figures/How_to_create_and_save_figures.R
+++ Scripts/Figures/How_to_create_and_save_figures.R
@@ -7,6 +7,9 @@ y <- 1:10
 # width and height can be changed, they're in nb of pixels
 jpeg("Figures/Test_image.jpg", width = 350, height = 350)
 # make your plot
-plot(x ~ y)
+ggplot(data = final_dataset) +
+	geom_jitter(aes(x = park.area.percentage, y = spc_richness, col=quarantine)) +
+	theme_classic() + 
+	labs(x="Percentage of park area", y="Species richness")
 # close the plot to save it.
 dev.off()
